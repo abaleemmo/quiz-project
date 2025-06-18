2ff2216a04f5f9c4321c95c22a2d8401de937427
Abdul-Aleem Mohammed <abaleemmohammed@gmail.com>
2025-06-18 09:17:37 -0500

    flower pieces done except petals

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ let mouth = document.querySelector(".flower__nose");
 let stem = document.querySelector(".flower__stem");
 let face = document.querySelector(".flower__head");
 let pot = document.querySelector(".flower__pot");
+let potShadow = document.querySelector(".flower__pot--shadow");
+let potBottom = document.querySelector(".flower__pot--bottom");
 let petals = document.querySelector(".flower__petal");
 
 
@@ -46,16 +48,21 @@ whiteFaceButton.addEventListener("click", function(){
 });
 lightBrownFaceButton.addEventListener("click", function(){
     faceColor = "Light Brown";
-    face.style.background = "lightbrown";
+    face.style.background = "#C4A484";
 });
 
 
 blackPotButton.addEventListener("click", function(){
     pot.style.background = "Black";
+    pot.style.boxShadow = "Black";
+    potShadow.style.background = "Black";
+    potBottom.style.background = "Black";
 });
 brownPotButton.addEventListener("click", function(){
     pot.style.background = "#FF7043";
     pot.style.boxShadow = "#FF7043";
+    potShadow.style.background = "#FF7043";
+    potBottom.style.background = "#FF7043";
 });
 
 
